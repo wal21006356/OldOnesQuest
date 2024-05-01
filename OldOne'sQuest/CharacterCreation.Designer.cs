d929370b8aea6efc659197b2d52ef0e3e3998d64
wal21006356 <wal21006356@students.wnc.ac.uk>
2024-05-01 11:07:40 +0100

    Add project files.

---
+++ OldOne'sQuest/CharacterCreation.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace OldOne_sQuest
+{
+    partial class CharacterCreation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CharacterCreation));
+            this.btnEarth = new System.Windows.Forms.Button();
+            this.btnWater = new System.Windows.Forms.Button();
+            this.btnFire = new System.Windows.Forms.Button();
+            this.btnAir = new System.Windows.Forms.Button();
+            this.btnName2 = new System.Windows.Forms.Button();
+            this.btnName3 = new System.Windows.Forms.Button();
+            this.btnName1 = new System.Windows.Forms.Button();
+            this.btnHealthUp = new System.Windows.Forms.Button();
+            this.btnWisdomUp = new System.Windows.Forms.Button();
+            this.btnDexterityUp = new System.Windows.Forms.Button();
+            this.btnHealthDown = new System.Windows.Forms.Button();
+            this.btnWisdomDown = new System.Windows.Forms.Button();
+            this.btnDexterityDown = new System.Windows.Forms.Button();
+            this.lblMaxHP = new System.Windows.Forms.Label();
+            this.lblWisdom = new System.Windows.Forms.Label();
+            this.lblDexterity = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnEarth
+            // 
+            this.btnEarth.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnEarth.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEarth.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEarth.ForeColor = System.Drawing.Color.White;
+            this.btnEarth.Location = new System.Drawing.Point(183, 153);
+            this.btnEarth.Name = "btnEarth";
+            this.btnEarth.Size = new System.Drawing.Size(92, 29);
+            this.btnEarth.TabIndex = 0;
+            this.btnEarth.Text = "Earth";
+            this.btnEarth.UseVisualStyleBackColor = false;
+            // 
+            // btnWater
+            // 
+            this.btnWater.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnWater.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnWater.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWater.ForeColor = System.Drawing.Color.Navy;
+            this.btnWater.Location = new System.Drawing.Point(283, 153);
+            this.btnWater.Name = "btnWater";
+            this.btnWater.Size = new System.Drawing.Size(92, 29);
+            this.btnWater.TabIndex = 1;
+            this.btnWater.Text = "Water";
+            this.btnWater.UseVisualStyleBackColor = false;
+            // 
+            // btnFire
+            // 
+            this.btnFire.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnFire.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFire.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFire.ForeColor = System.Drawing.Color.Maroon;
+            this.btnFire.Location = new System.Drawing.Point(384, 153);
+            this.btnFire.Name = "btnFire";
+            this.btnFire.Size = new System.Drawing.Size(92, 29);
+            this.btnFire.TabIndex = 2;
+            this.btnFire.Text = "Fire";
+            this.btnFire.UseVisualStyleBackColor = false;
+            // 
+            // btnAir
+            // 
+            this.btnAir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnAir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAir.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAir.ForeColor = System.Drawing.Color.Black;
+            this.btnAir.Location = new System.Drawing.Point(484, 153);
+            this.btnAir.Name = "btnAir";
+            this.btnAir.Size = new System.Drawing.Size(92, 29);
+            this.btnAir.TabIndex = 3;
+            this.btnAir.Text = "Air";
+            this.btnAir.UseVisualStyleBackColor = false;
+            // 
+            // btnName2
+            // 
+            this.btnName2.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnName2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnName2.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnName2.ForeColor = System.Drawing.Color.Black;
+            this.btnName2.Location = new System.Drawing.Point(309, 76);
+            this.btnName2.Name = "btnName2";
+            this.btnName2.Size = new System.Drawing.Size(141, 31);
+            this.btnName2.TabIndex = 4;
+            this.btnName2.Text = "NameOption2";
+            this.btnName2.UseVisualStyleBackColor = false;
+            // 
+            // btnName3
+            // 
+            this.btnName3.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnName3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnName3.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnName3.ForeColor = System.Drawing.Color.Black;
+            this.btnName3.Location = new System.Drawing.Point(456, 76);
+            this.btnName3.Name = "btnName3";
+            this.btnName3.Size = new System.Drawing.Size(141, 31);
+            this.btnName3.TabIndex = 5;
+            this.btnName3.Text = "NameOption3";
+            this.btnName3.UseVisualStyleBackColor = false;
+            // 
+            // btnName1
+            // 
+            this.btnName1.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnName1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnName1.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnName1.ForeColor = System.Drawing.Color.Black;
+            this.btnName1.Location = new System.Drawing.Point(162, 76);
+            this.btnName1.Name = "btnName1";
+            this.btnName1.Size = new System.Drawing.Size(141, 31);
+            this.btnName1.TabIndex = 6;
+            this.btnName1.Text = "NameOption1";
+            this.btnName1.UseVisualStyleBackColor = false;
+            // 
+            // btnHealthUp
+            // 
+            this.btnHealthUp.BackColor = System.Drawing.Color.Lime;
+            this.btnHealthUp.FlatAppearance.BorderSize = 0;
+            this.btnHealthUp.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnHealthUp.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHealthUp.Location = new System.Drawing.Point(484, 233);
+            this.btnHealthUp.Name = "btnHealthUp";
+            this.btnHealthUp.Size = new System.Drawing.Size(29, 29);
+            this.btnHealthUp.TabIndex = 7;
+            this.btnHealthUp.Text = ">";
+            this.btnHealthUp.UseVisualStyleBackColor = false;
+            this.btnHealthUp.Click += new System.EventHandler(this.btnHealthUp_Click);
+            // 
+            // btnWisdomUp
+            // 
+            this.btnWisdomUp.BackColor = System.Drawing.Color.Lime;
+            this.btnWisdomUp.FlatAppearance.BorderSize = 0;
+            this.btnWisdomUp.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnWisdomUp.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWisdomUp.Location = new System.Drawing.Point(484, 268);
+            this.btnWisdomUp.Name = "btnWisdomUp";
+            this.btnWisdomUp.Size = new System.Drawing.Size(29, 29);
+            this.btnWisdomUp.TabIndex = 8;
+            this.btnWisdomUp.Text = ">";
+            this.btnWisdomUp.UseVisualStyleBackColor = false;
+            this.btnWisdomUp.Click += new System.EventHandler(this.btnWisdomUp_Click);
+            // 
+            // btnDexterityUp
+            // 
+            this.btnDexterityUp.BackColor = System.Drawing.Color.Lime;
+            this.btnDexterityUp.FlatAppearance.BorderSize = 0;
+            this.btnDexterityUp.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnDexterityUp.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDexterityUp.Location = new System.Drawing.Point(484, 303);
+            this.btnDexterityUp.Name = "btnDexterityUp";
+            this.btnDexterityUp.Size = new System.Drawing.Size(29, 29);
+            this.btnDexterityUp.TabIndex = 9;
+            this.btnDexterityUp.Text = ">";
+            this.btnDexterityUp.UseVisualStyleBackColor = false;
+            this.btnDexterityUp.Click += new System.EventHandler(this.btnDexterityUp_Click);
+            // 
+            // btnHealthDown
+            // 
+            this.btnHealthDown.BackColor = System.Drawing.Color.OrangeRed;
+            this.btnHealthDown.FlatAppearance.BorderSize = 0;
+            this.btnHealthDown.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnHealthDown.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHealthDown.Location = new System.Drawing.Point(246, 233);
+            this.btnHealthDown.Name = "btnHealthDown";
+            this.btnHealthDown.Size = new System.Drawing.Size(29, 29);
+            this.btnHealthDown.TabIndex = 10;
+            this.btnHealthDown.Text = "<";
+            this.btnHealthDown.UseVisualStyleBackColor = false;
+            this.btnHealthDown.Click += new System.EventHandler(this.btnHealthDown_Click);
+            // 
+            // btnWisdomDown
+            // 
+            this.btnWisdomDown.BackColor = System.Drawing.Color.OrangeRed;
+            this.btnWisdomDown.FlatAppearance.BorderSize = 0;
+            this.btnWisdomDown.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnWisdomDown.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWisdomDown.Location = new System.Drawing.Point(246, 268);
+            this.btnWisdomDown.Name = "btnWisdomDown";
+            this.btnWisdomDown.Size = new System.Drawing.Size(29, 29);
+            this.btnWisdomDown.TabIndex = 11;
+            this.btnWisdomDown.Text = "<";
+            this.btnWisdomDown.UseVisualStyleBackColor = false;
+            this.btnWisdomDown.Click += new System.EventHandler(this.btnWisdomDown_Click);
+            // 
+            // btnDexterityDown
+            // 
+            this.btnDexterityDown.BackColor = System.Drawing.Color.OrangeRed;
+            this.btnDexterityDown.FlatAppearance.BorderSize = 0;
+            this.btnDexterityDown.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnDexterityDown.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDexterityDown.Location = new System.Drawing.Point(246, 303);
+            this.btnDexterityDown.Name = "btnDexterityDown";
+            this.btnDexterityDown.Size = new System.Drawing.Size(29, 29);
+            this.btnDexterityDown.TabIndex = 12;
+            this.btnDexterityDown.Text = "<";
+            this.btnDexterityDown.UseVisualStyleBackColor = false;
+            this.btnDexterityDown.Click += new System.EventHandler(this.btnDexterityDown_Click);
+            // 
+            // lblMaxHP
+            // 
+            this.lblMaxHP.AutoSize = true;
+            this.lblMaxHP.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMaxHP.Location = new System.Drawing.Point(325, 238);
+            this.lblMaxHP.Name = "lblMaxHP";
+            this.lblMaxHP.Size = new System.Drawing.Size(107, 20);
+            this.lblMaxHP.TabIndex = 13;
+            this.lblMaxHP.Text = "Max Health: 0";
+            this.lblMaxHP.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblWisdom
+            // 
+            this.lblWisdom.AutoSize = true;
+            this.lblWisdom.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWisdom.Location = new System.Drawing.Point(340, 273);
+            this.lblWisdom.Name = "lblWisdom";
+            this.lblWisdom.Size = new System.Drawing.Size(80, 20);
+            this.lblWisdom.TabIndex = 14;
+            this.lblWisdom.Text = "Wisdom: 0";
+            this.lblWisdom.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblDexterity
+            // 
+            this.lblDexterity.AutoSize = true;
+            this.lblDexterity.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDexterity.Location = new System.Drawing.Point(330, 308);
+            this.lblDexterity.Name = "lblDexterity";
+            this.lblDexterity.Size = new System.Drawing.Size(95, 20);
+            this.lblDexterity.TabIndex = 15;
+            this.lblDexterity.Text = "Dexterity: 0";
+            this.lblDexterity.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // CharacterCreation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblDexterity);
+            this.Controls.Add(this.lblWisdom);
+            this.Controls.Add(this.lblMaxHP);
+            this.Controls.Add(this.btnDexterityDown);
+            this.Controls.Add(this.btnWisdomDown);
+            this.Controls.Add(this.btnHealthDown);
+            this.Controls.Add(this.btnDexterityUp);
+            this.Controls.Add(this.btnWisdomUp);
+            this.Controls.Add(this.btnHealthUp);
+            this.Controls.Add(this.btnName1);
+            this.Controls.Add(this.btnName3);
+            this.Controls.Add(this.btnName2);
+            this.Controls.Add(this.btnAir);
+            this.Controls.Add(this.btnFire);
+            this.Controls.Add(this.btnWater);
+            this.Controls.Add(this.btnEarth);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "CharacterCreation";
+            this.Text = "Character Creation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEarth;
+        private System.Windows.Forms.Button btnWater;
+        private System.Windows.Forms.Button btnFire;
+        private System.Windows.Forms.Button btnAir;
+        private System.Windows.Forms.Button btnName2;
+        private System.Windows.Forms.Button btnName3;
+        private System.Windows.Forms.Button btnName1;
+        private System.Windows.Forms.Button btnHealthUp;
+        private System.Windows.Forms.Button btnWisdomUp;
+        private System.Windows.Forms.Button btnDexterityUp;
+        private System.Windows.Forms.Button btnHealthDown;
+        private System.Windows.Forms.Button btnWisdomDown;
+        private System.Windows.Forms.Button btnDexterityDown;
+        private System.Windows.Forms.Label lblMaxHP;
+        private System.Windows.Forms.Label lblWisdom;
+        private System.Windows.Forms.Label lblDexterity;
+    }
+}
+
---
+++ OldOne'sQuest/CharacterCreation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OldOne_sQuest
+{
+    public partial class CharacterCreation : Form
+    {
+        public CharacterCreation()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHealthDown_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnHealthUp_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnWisdomDown_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnWisdomUp_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDexterityDown_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDexterityUp_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
